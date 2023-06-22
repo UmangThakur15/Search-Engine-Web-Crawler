4bd0b413380949aff3f472126b1f0d38605d2d1b
Umang Thakur <84286829+UmangThakur15@users.noreply.github.com>
2023-06-21 21:45:17 -0400

    Add files via upload

---
+++ ranking.py
@@ -0,0 +1,61 @@
+from elasticsearch import Elasticsearch
+import json
+
+index = 'mass_gov'
+hosts = ["https://umang.kb.us-central1.gcp.cloud.es.io:9243/"]
+cloud_id ='Umang:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvOjQ0MyQ0MTcwYmM4NzNhZWY0Y2ZhODc5NWZjMjFiNmU2YWFjMyRjNmRlMDljODE0Nzc0YTdiYjJlZTdmYjkxOTk3N2UyMg=='
+es = Elasticsearch(request_timeout=10000, cloud_id=cloud_id, http_auth=('elastic', "CrX7HTtr0xUnS0KUQ8PcnQHd"))
+
+
+
+
+
+query_id = []
+query = []
+score= []
+id =[]
+
+
+def access_query(query,query_id):
+    #f=open(filename)
+    #data = json.load(f)
+    print("query",query)
+    print("query_id",query_id)
+    e = 400
+    d=200
+    c=0
+    list = []
+    file = open('output_gen.txt', 'a')
+    query_id=query_id
+    query=query
+    #client = Elasticsearch()
+    index = 'mass_gov'
+    doc = {
+            "size": 200,
+            "query": {
+                "match": {"content": query
+                              }
+                }
+            }
+    resp = es.search(index=index,
+                             body=doc)
+    print("resp",resp)
+    for hit in resp['hits']['hits']:
+        e = e + 1
+        #print(c)
+        #print(key,"Q0" ,hit["_id"],c,hit["_score"],"Exp")
+        list.append(str(query_id)+" Q0 "+str(hit["_id"])+" "+str(e)+" "+str(hit["_score"])+" Exp")
+    print(list)
+    file = open('output_gen.txt', 'a')
+    for item in list:
+        file.write(item + "\n")
+    file.close()
+
+
+def main():
+    query = "elizabeth warren platform"
+    query_id = 150203
+    access_query(query,query_id)
+    #output_file(query_id, id,score,rank)
+
+main()
